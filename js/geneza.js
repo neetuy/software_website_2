45c1ff2ee82af147282bd7e800df15cd82c22525
neetuy <neetuy1455@gmail.com>
2017-02-15 17:33:48 +0530

    modified

--- js/geneza.js
+++ js/geneza.js
@@ -13,9 +13,34 @@
             );
             $("button").click(function(event){
                $(".toggle").toggle('slow', function(){
-                 
+               
                });
             });
 
+               // $(function(){
+               // $('fadeIn').fadeIn(2000);
+               // setTimeout(function(){
+               //  $('.fadeIn').fadeOut(2000, function(){
+               //    location.reload(true);
+               //  });
+               // } 60000);
+               // });
+                 faders = $(".fadeIn").fadeTo(0,1);
+                $(window).scroll(function(){
+                    faders.each(function(){
+                        a = $(this).offset().top + $(this).height();
+                        b = $(window).scrollTop() + ($(window).height());
+                        c = $(window).scrollTop() + $(this).height();
+                        if (c > $(this).offset().top){
+                            $(this).fadeTo(0,1);
+                        }
+                        else if (a < b) {
+                            $(this).fadeTo(0,1);
+                        }
+                        else { 
+                            $(this).fadeTo(0,0);
+                        }
+                    });
+                });
    });
 
